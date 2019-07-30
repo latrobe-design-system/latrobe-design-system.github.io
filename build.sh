ed4aa198785a71fd35799eb08da2e2c85ed7260d
Albert Penticoss <a.penticoss@latrobe.edu.au>
2019-07-30 12:00:30 +1000

    Sample text change

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #compile css for website
 echo -e "\033[0;32mCompile sass...\033[0m"
-sass --style=compressed ltucss/_sass:ltucss/css
+sass --style=expanded ltucss/_sass:ltucss/css
 
 #build Site
 echo -e "\033[0;32mBuilding site...\033[0m"
