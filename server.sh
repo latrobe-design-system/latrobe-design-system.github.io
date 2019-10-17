b962463e18a3a5ece21ac5a106da38ec6b327473
Albert Penticoss <a.penticoss@latrobe.edu.au>
2019-10-18 07:53:30 +1100

    Remove box shadow on vertical nav when nav-out

--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@
 
 echo -e "\033[0;32mStarting sass watch and jekyll server...\033[0m"
 
-sass --watch --style=compressed ltu-patterns/assets/_sass:ltu-patterns/assets/css & bundle exec jekyll serve
+sass --watch --style=compressed ltu-patterns/assets/_sass:ltu-patterns/assets/css & bundle exec jekyll serve & fg
