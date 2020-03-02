88e1f94bf6acf29e9d0c2d1aa8489e50b1ed8f4d
Albert Penticoss <a.penticoss@latrobe.edu.au>
2020-03-02 11:04:39 +1100

    Remove old serve and build scripts

--- server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -e "\033[0;32mStarting sass watch and jekyll server...\033[0m"
-
-sass --watch --style=compressed _ltu-components/src/ds-ltu.scss:_ltu-components/dist/css/ds-ltu.css & bundle exec jekyll serve
