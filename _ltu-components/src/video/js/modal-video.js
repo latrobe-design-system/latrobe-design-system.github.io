7cdca16afe6c3ad43bdbbe89b522a261c39a0169
Albert Penticoss <a.penticoss@latrobe.edu.au>
2020-05-22 15:42:32 +1000

    Fix modal video html and css

--- _ltu-components/src/video/js/modal-video.js
+++ _ltu-components/src/video/js/modal-video.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function($) {
         var videoId = $(target).attr('data-youtube');
 
         // modal content
-        var modalContent = '<div class="ds-video"><iframe id="vid-'+videoId+'" width="560" height="315" type="text/html" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
+        var modalContent = '<div class="ds-video ds-video--fullwidth"><div class="ds-video__wrapper"><iframe id="vid-'+videoId+'" width="560" height="315" type="text/html" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe></div></div>';
 
         // open modal with responsive video content
         openModal(target, modalContent);
