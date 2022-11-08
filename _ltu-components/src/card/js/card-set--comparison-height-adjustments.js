bacdfd10349f35476b1ec307d203f62c5356215d
Siang Lim <sianglim@outlook.com>
2022-11-08 14:27:59 +1100

    Comparison cards cleanup and fixes

--- _ltu-components/src/card/js/card-set--comparison-height-adjustments.js
+++ _ltu-components/src/card/js/card-set--comparison-height-adjustments.js
@@ -1,102 +1,84 @@
-let tallestCardHeading = 0;
-let tallestCardInfo = 0;
-let tallestCardStatusInfo = 0;
-let numberOfAccordionsPerCard = 0;
-let tallestAccordionInSet = [];
-
-$(document).ready(function() {
-    console.log('height init')
-    
-    const cardElementSet = document.querySelectorAll('.ds-card-set--comparison .ds-card');
+$(document).ready(function() {   
+    dynamicHeights();
 
     addEventListener('resize', (event) => {
-        console.log(event, 'resized!')
-        cardElementSet.forEach(element => {
-            console.log(element)
-            const cardHeading = element.querySelector('.ds-card__heading')
-            const cardStatusInfo = element.querySelector('.ds-card__status-info')
-            const cardInfo = element.querySelector('.ds-card__info');
-            const cardAccordionSet = element.querySelectorAll('.ds-accordion');
-    
-            if (cardHeading.clientHeight > tallestCardHeading) {
-                tallestCardHeading = cardHeading.clientHeight;
-            }
-    
-            if (cardStatusInfo.clientHeight > tallestCardStatusInfo) {
-                tallestCardStatusInfo = cardStatusInfo.clientHeight;
-            }
-    
-            if (cardInfo.clientHeight > tallestCardInfo) {
-                tallestCardInfo = cardInfo.clientHeight;
-            }
-
-            numberOfAccordionsPerCard = cardAccordionSet.length;
-            console.log(numberOfAccordionsPerCard)
-
-            cardAccordionSet.forEach((accordion, index) => {
-                // accordion.style.height = tallestAccordionInSet + 'px';
-                console.log('accordion', accordion);
-                // if (tallestAccordionInSet[index] === null) {
-                //     console.log('null');
-                //     tallestAccordionInSet[index] = 0
-                // }
-
-                tallestAccordionInSet.push(accordion.clientHeight);
-                if (accordion.clientHeight > tallestAccordionInSet[index]) {
-                    tallestAccordionInSet[index] = accordion.clientHeight;
-                }
-            })
-
-            console.log('tallest', tallestAccordionInSet);
-        })
-    
-        cardElementSet.forEach(element => {
-            const cardHeading = element.querySelector('.ds-card__heading')
-            const cardStatusInfo = element.querySelector('.ds-card__status-info')
-            const cardInfo = element.querySelector('.ds-card__info');
-            const cardAccordionSet = element.querySelectorAll('.ds-accordion');
-    
-            cardHeading.style.height = tallestCardHeading + 'px';
-            cardStatusInfo.style.height = tallestCardStatusInfo + 'px';
-            cardInfo.style.height = tallestCardInfo + 'px';
-            
-            cardAccordionSet.forEach((accordion, index) => {
-                // let accordionHeading = accordion.querySelector('h3');
-                // accordionHeading.style.height = tallestAccordionInSet[index] + 'px';
-                accordion.style.height = tallestAccordionInSet[index] + 'px';
-            })
-        })
-    
-    });
-
-
-    // cardHeading.forEach(element => {
-    //     let height = element.clientHeight;
-    //     console.log(height)
-    //     if (height > tallestCardHeading) {
-    //         tallestCardHeading = height;
-    //     }
-
-    //     console.log('tallest card heading is now', tallestCardHeading)
-    // })
-
-    // cardInfo.forEach(element => {
-    //     let height = element.clientHeight;
-    //     console.log(height)
-    //     if (height > tallestCardInfo) {
-    //         tallestCardInfo = height;
-    //     }
-
-    //     console.log('tallest card info is now', tallestCardInfo)
-    // })
-
-    // cardStatusInfo.forEach(element => {
-    //     let height = element.clientHeight;
-    //     console.log(height)
-    //     if (height > tallestCardStatusInfo) {
-    //         tallestCardStatusInfo = height;
-    //     }
-
-    //     console.log('tallest card info status is now', tallestCardStatusInfo)
-    // })
+        dynamicHeights();
+    })
 })
+
+function dynamicHeights() {
+    let tallestCardHeading = 0;
+    let tallestCardInfo = 0;
+    let tallestCardStatusInfo = 0;
+    let tallestCardChecklist = 0;
+    let numberOfAccordionsPerCard = 0;
+    let tallestAccordionInSet = [];
+
+    const cardElementSet = document.querySelectorAll('.ds-card-set--comparison .ds-card');
+
+    cardElementSet.forEach(element => {
+        const cardHeading = element.querySelector('.ds-card__heading')
+        const cardStatusInfo = element.querySelector('.ds-card__status-info')
+        const cardInfo = element.querySelector('.ds-card__info');
+        const cardChecklist = element.querySelector('.ds-card__checklist');
+        const cardAccordionSet = element.querySelectorAll('.ds-accordion .ds-accordion__trigger');
+
+        if (cardHeading.clientHeight > tallestCardHeading) {
+            tallestCardHeading = cardHeading.clientHeight;
+        }
+
+        if (cardStatusInfo.clientHeight > tallestCardStatusInfo) {
+            tallestCardStatusInfo = cardStatusInfo.clientHeight;
+        }
+
+        if (cardInfo.clientHeight > tallestCardInfo) {
+            tallestCardInfo = cardInfo.clientHeight;
+        }
+
+        if (cardChecklist.clientHeight > tallestCardChecklist) {
+            tallestCardChecklist = cardChecklist.clientHeight;
+        }
+
+        cardAccordionSet.forEach((accordion, index) => {
+            if ( tallestAccordionInSet[index] === undefined ) {
+                if (accordion.clientHeight > 50) {                        
+                    tallestAccordionInSet.push(accordion.clientHeight);
+                }
+            }
+
+            if ( accordion.clientHeight > tallestAccordionInSet[index] ) {
+                tallestAccordionInSet[index] = accordion.clientHeight;
+            }
+        })
+    })
+
+    cardElementSet.forEach(element => {
+        const cardHeading = element.querySelector('.ds-card__heading')
+        const cardStatusInfo = element.querySelector('.ds-card__status-info')
+        const cardInfo = element.querySelector('.ds-card__info');
+        const cardChecklist = element.querySelector('.ds-card__checklist');
+        const cardAccordionSet1 = element.querySelectorAll('.ds-accordion');
+        const cardAccordionSet = element.querySelectorAll('.ds-accordion  .ds-accordion__trigger');
+
+        cardHeading.style.height = '';
+        cardHeading.style.height = tallestCardHeading + 'px';
+
+        cardStatusInfo.style.height = '';
+        cardStatusInfo.style.height = tallestCardStatusInfo + 'px';
+
+        cardInfo.style.height = '';
+        cardInfo.style.height = tallestCardInfo + 'px';
+
+        cardChecklist.style.height = '';
+        cardChecklist.style.height = tallestCardChecklist + 'px';
+        
+        cardAccordionSet1.forEach((accordion, index) => {
+            accordion.style.height = '';
+            accordion.style.height = tallestAccordionInSet[index] + 'px';
+        })
+        cardAccordionSet.forEach((accordion, index) => {
+            accordion.style.height = '';
+            accordion.style.height = tallestAccordionInSet[index] + 'px';
+        })
+    })
+}
