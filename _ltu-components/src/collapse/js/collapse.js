e53c76ede847195ea04a2dae8e86196f87c9838e
Ethan <ethan-ho@users.noreply.github.com>
2023-01-24 12:05:04 +1100

    Support collapse in modal (#311)
    * support collapse placed inside modal
    
    Co-authored-by: Ethan Ho <f.ho@latrobe.edu.au>

--- _ltu-components/src/collapse/js/collapse.js
+++ _ltu-components/src/collapse/js/collapse.js
@@ -4,14 +4,24 @@ jQuery(document).ready(function ($) {
         selectors: {
             container: '',
             children: {
-                close: '.ds-collapse__close',
                 trigger: '.ds-collapse__trigger',
             }
         },
     }
 
+    function getSelectorPrefix(elem) {
+        let selectorPrefix = ''
+
+        if (!!elem.closest('.ds-modal')) {
+            selectorPrefix = '.ds-modal '
+        }
+
+        return selectorPrefix;
+    }
+
     function triggerOnClick(event) {
-        var regionId = $(event.currentTarget).attr('aria-controls');
+        const regionId = $(event.currentTarget).attr('aria-controls');
+        const selectorPrefix = getSelectorPrefix(event.currentTarget)
 
         if ($(event.currentTarget).attr('aria-expanded') == 'false') { // region is collapsed then open it
 
@@ -19,42 +29,32 @@ jQuery(document).ready(function ($) {
             $(event.currentTarget).attr('aria-expanded', 'true');
 
             // open the region
-            $('#' + regionId).slideDown(function () {
+            $(`${selectorPrefix}[id="${regionId}"]`).slideDown(function () {
                 $(event.currentTarget).removeAttr('hidden');
             });
 
-        }
-        else { // region is expanded then close it
+        } else { // region is expanded then close it
 
             // update the aria-expanded attribute of the clicked triiger
             $(event.currentTarget).attr('aria-expanded', 'false');
 
             // close the region
-            $('#' + regionId).slideUp(function () {
+            $(`${selectorPrefix}[id="${regionId}"]`).slideUp(function () {
                 $(event.currentTarget).attr('hidden', 'hidden');
             });
         }
     }
 
-    function closeOnClick(event) {
-        var regionId = $(event.currentTarget).attr('aria-controls');
-        $('[aria-controls="' + regionId + '"]').trigger('click').focus();
-    }
-
     window.LTUDS.registerComponent(_config.name, {
         eventHandlers: {
             triggerOnClick: triggerOnClick,
-            closeOnClick: closeOnClick,
         },
         setup: (helper) => {
             const { getSelector } = helper
 
-            const closeSelector = getSelector(_config.selectors.container, 'close', _config.selectors)
             const triggerSelector = getSelector(_config.selectors.container, 'trigger', _config.selectors)
 
             jQuery(document.body)
-                .off('click', closeSelector, closeOnClick)
-                .on('click', closeSelector, closeOnClick)
                 .off('click', triggerSelector, triggerOnClick)
                 .on('click', triggerSelector, triggerOnClick)
                 ;
